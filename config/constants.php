1ee9505f69aaf6ca003b4c6317f3d060cf32a5d0
sujitdohalek5 <50798921+sujitdohalek5@users.noreply.github.com>
2023-09-13 11:45:27 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -6,11 +6,11 @@
     defined('SMTP_HOST')        OR define('SMTP_HOST', 'ssl://smtp.googlemail.com');
     defined('SMTP_PORT')        OR define('SMTP_PORT', '465');
     defined('SMTP_TIMEOUT')     OR define('SMTP_TIMEOUT', '30');
-    defined('SMTP_USER')        OR define('SMTP_USER', 'innovac.gagan@gmail.com');
-    defined('FORM')             OR define('FORM', 'innovac.gagan@gmail.com');
-    defined('CC')               OR define('CC', 'gaganbansode@gmail.com');
-    defined('FORM_NAME')        OR define('FORM_NAME', 'Innovac ');
-    defined('SMTP_PASS')        OR define('SMTP_PASS', 'gagan@123');
+    defined('SMTP_USER')        OR define('SMTP_USER', '');
+    defined('FORM')             OR define('FORM', '');
+    defined('CC')               OR define('CC', '');
+    defined('FORM_NAME')        OR define('FORM_NAME', ' ');
+    defined('SMTP_PASS')        OR define('SMTP_PASS', '');
     defined('CHARSET')          OR define('CHARSET', 'utf-8');
     defined('MAILTYPE')         OR define('MAILTYPE', 'html');
     defined('EMAIL_FLAG')       OR define('EMAIL_FLAG', 1);
